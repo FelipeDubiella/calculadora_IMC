c4e404492c08262e6ec84d1a2c1b232a30b26123
Felipe Dubiella de Oliveira <Dubiella2@icloud.com>
2024-06-22 11:31:40 -0300

    feat: add logic for BMI calculation
    Implemented the logic to calculate the Body Mass Index (BMI) based on weight and height inputs. The function returns the BMI value and includes basic error handling for invalid inputs.

--- app/src/main/java/com/felipedubiella/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/felipedubiella/calculadora_imc/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.felipedubiella.calculadora_imc
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.felipedubiella.calculadora_imc.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener {
+
+            val height : Float = binding.edtHeight.text.toString().toFloat()
+            val weight : Float = binding.edtWeight.text.toString().toFloat()
+
+            val heightQ2 = height * height
+            val result = weight/heightQ2
+
+            println("Seu IMC é: " + result)
+
+        }
 
 
 
